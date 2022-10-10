3b1f8eef38f3981916f12c8161edef54fbf591ce
soytmen <114388813+soytmen@users.noreply.github.com>
2022-10-10 10:41:05 +0200

    01 script

---
+++ Assets/Scripts/MyFirstScrip.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScrip : MonoBehaviour
+{
+    public int playerAge = 33;
+    private float playerSpeed = 3.125f;
+    public string playerName = "Miguel";
+    [SerializeField] private bool gameOver = false;
+
+}
